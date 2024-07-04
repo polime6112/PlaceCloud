ff8994b956040cf2f1f750b023c57bb3c49cb328
sec <sec@KJY>
2024-07-04 11:46:53 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/polime6112/PlaceCloud.git

---
+++ src/main/java/web/spring/placecloud/service/LikeServiceImple.java
@@ -0,0 +1,44 @@
+package web.spring.placecloud.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.log4j.Log4j;
+import web.spring.placecloud.domain.LikeVO;
+import web.spring.placecloud.persistence.LikeMapper;
+
+@Service
+@Log4j
+public class LikeServiceImple implements LikeService {
+	
+	@Autowired
+	private LikeMapper likeMapper;
+	
+	@Override
+	public int createBoard(LikeVO likeVO) {
+		log.info("createBoard()");
+		int result = likeMapper.insert(likeVO);
+		return result;
+	}
+
+	@Override
+	public List<LikeVO> getAllBoards() {
+		log.info("getAllBoards()");
+		return likeMapper.selectList();
+	}
+
+	@Override
+	public LikeVO getBoardbyId(int placeId, String memberEmail) {
+		log.info("getBoardById()");
+		return likeMapper.selectOne(placeId, memberEmail);
+	}
+
+	@Override
+	public int deleteBoard(int placeId, String memberEmail) {
+		log.info("deleteBoard()");
+		return likeMapper.delete(placeId, memberEmail);
+	}
+
+}
