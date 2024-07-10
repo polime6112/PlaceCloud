a84a5a4bf07e85bc5d99a277edf691fc6ea9a62b
sec <sec@KJY>
2024-07-10 14:15:14 +0900

    07.09 김주영 주석 수정 밑 일부 추가

--- src/main/java/web/spring/placecloud/service/KakaoPayService.java
+++ src/main/java/web/spring/placecloud/service/KakaoPayService.java
@@ -30,6 +30,7 @@ public class KakaoPayService {
 	
 	private String tid;
 	
+	// 결제에 필요한 정보를 api양식에 맞게 호출하여 결제 준비(ready) 후 사용자에게 결제 승인 요청
 	public KakaoPayReadyResponse ready(BookingVO bookingVO) {
 		log.info("bookingVO : " + bookingVO);
 		
@@ -68,8 +69,11 @@ public class KakaoPayService {
 		return readyResponse;
 	} // end ready()
 	
+	// 사용자에게 결제 요청을 승인 하면 결제한 정보를 바탕으로 결제 완료 후 알림톡 송신
 	public String approve(BookingVO bookingVO, String pgToKen) {
 		// ready()를 진행할 때 저장해 놓은 TID로 승인 요청
+		
+		// Request header
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Authorization", "DEV_SECRET_KEY " + kakaopaySecretKey);
 		headers.setContentType(MediaType.APPLICATION_JSON);
@@ -93,11 +97,13 @@ public class KakaoPayService {
 			
 			// 승인 결과를 저장
 			String approveResponse = response.getBody();
+			log.info("approve()end");
 			return approveResponse;
 		} catch (HttpStatusCodeException ex) {
+			log.info("approve()end");
 			return ex.getResponseBodyAsString();
 		}
-		
 	}
+	
 	
 }
